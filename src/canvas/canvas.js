9329da152d4b58299ba16b73f28e48fae638cad7
Overflow <77083630+KaneSeries2@users.noreply.github.com>
2021-03-24 17:08:24 -0400

    paint variables

--- src/canvas/canvas.js
+++ src/canvas/canvas.js
@@ -1,7 +1,13 @@
 window.addEventListener('load', ()=>{
     const canvas = document.querySelector('#canvas');
     const ctx = canvas.getContext("2d");
-
+    const frame = document.getElementById("img-display");
     //Resizing
+    canvas.width = window.innerWidth
+    canvas.height = window.innerHeight
     
+    //VARIABLES
+    let painting = false;
+
+    //EVENT  LISTENERS
 })
