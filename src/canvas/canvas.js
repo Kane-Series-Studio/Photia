5bb5150145c3e104b4880f6c125ff3518aea016f
Overflow <77083630+KaneSeries2@users.noreply.github.com>
2021-03-24 17:12:29 -0400

    mouse up event listener

--- src/canvas/canvas.js
+++ src/canvas/canvas.js
@@ -1,21 +1,22 @@
-window.addEventListener('load', ()=>{
+window.addEventListener('load', () => {
     const canvas = document.querySelector('#canvas');
     const ctx = canvas.getContext("2d");
     //Resizing
     canvas.width = window.innerWidth
     canvas.height = window.innerHeight
-    
+
     //VARIABLES
     let painting = false;
 
-    function startPosition(){
+    function startPosition() {
         painting = true;
     }
-    function finishedPosition(){
+    function finishedPosition() {
         painting = false;
     }
 
     //EVENT  LISTENERS
-    canvas.addEventListener('mousedown', 
-    startPosition)
+    canvas.addEventListener('mousedown', startPosition);
+    canvas.addEventListener('mouseup', finishedPosition)
+
 })
