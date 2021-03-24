d459d49a8e409bcf06afc506cff37576ca953984
Overflow <77083630+KaneSeries2@users.noreply.github.com>
2021-03-24 14:07:11 -0400

    colour manager

--- src/index.js
+++ src/index.js
@@ -16,6 +16,7 @@ const createWindow = () => {
   });
 
   // and load the index.html of the app.
+  mainWindow.maximize()
   mainWindow.loadFile(path.join(__dirname, 'index.html'));
   mainWindow.setMenuBarVisibility(true)
 };
@@ -113,6 +114,18 @@ app.on('ready', function () {
           label: "Exit",
           role: "quit"
         },
+        {
+          type: 'separator'
+        },
+        { role: 'reload' },
+      { role: 'forceReload' },
+      { role: 'toggleDevTools' },
+      { type: 'separator' },
+      { role: 'resetZoom' },
+      { role: 'zoomIn' },
+      { role: 'zoomOut' },
+      { type: 'separator' },
+      { role: 'togglefullscreen' }
       ]
     }
   ]
@@ -120,11 +133,6 @@ app.on('ready', function () {
   Menu.setApplicationMenu(menu);
 })
 
-
-
-// Quit when all windows are closed, except on macOS. There, it's common
-// for applications and their menu bar to stay active until the user quits
-// explicitly with Cmd + Q.
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
     app.quit();
@@ -132,12 +140,9 @@ app.on('window-all-closed', () => {
 });
 
 app.on('activate', () => {
-  // On OS X it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
   if (BrowserWindow.getAllWindows().length === 0) {
     createWindow();
   }
 });
 
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and import them here.
+
