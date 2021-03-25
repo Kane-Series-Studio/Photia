fadf15be650258c42ab71fdf58d89cf75e8452da
Overflow <77083630+KaneSeries2@users.noreply.github.com>
2021-03-25 10:29:39 -0400

    canvas remake

--- src/canvas/canvas.js
+++ src/canvas/canvas.js
@@ -1,57 +1,114 @@
-window.addEventListener('load', () => {
+var canvas, ctx, flag = false,
+        prevX = 0,
+        currX = 0,
+        prevY = 0,
+        currY = 0,
+        dot_flag = false;
 
-    const canvas = document.querySelector('#canvas');
-    const ctx = canvas.getContext("2d");
-
-
-    // TRACK COLOUR CHANGE
-
-
-    // Stroke Size Caller
-    const stroke_size = document.getElementById("strokeSize").value;
-    setInterval(() => {
-        console.log(stroke_size)
-    }, 1);
-
-    //Resizing
-    canvas.width = window.innerWidth
-    canvas.height = window.innerHeight
-
-    //VARIABLES
-    let painting = false;
-
-    function startPosition(e) {
-        painting = true;
-        draw(e);
+    var x = "black",
+        y = 2;
+    
+    function init() {
+        canvas = document.getElementById('can');
+        ctx = canvas.getContext("2d");
+        w = canvas.width;
+        h = canvas.height;
+    
+        canvas.addEventListener("mousemove", function (e) {
+            findxy('move', e)
+        }, false);
+        canvas.addEventListener("mousedown", function (e) {
+            findxy('down', e)
+        }, false);
+        canvas.addEventListener("mouseup", function (e) {
+            findxy('up', e)
+        }, false);
+        canvas.addEventListener("mouseout", function (e) {
+            findxy('out', e)
+        }, false);
     }
-    function finishedPosition() {
-        painting = false;
+    
+    function color(obj) {
+        switch (obj.id) {
+            case "green":
+                x = "green";
+                break;
+            case "blue":
+                x = "blue";
+                break;
+            case "red":
+                x = "red";
+                break;
+            case "yellow":
+                x = "yellow";
+                break;
+            case "orange":
+                x = "orange";
+                break;
+            case "black":
+                x = "black";
+                break;
+            case "white":
+                x = "white";
+                break;
+        }
+        if (x == "white") y = 14;
+        else y = 2;
+    
+    }
+    
+    function draw() {
         ctx.beginPath();
-    }
-
-    function colour_change(){
-        console.log('CHANGED')
-    }
-
-    function draw(e) {
-        setInterval(() => {
-        var e = window.event;
-        var x = document.getElementById("colour-picker").value;
-        if (!painting) return;    
-        ctx.lineWidth = 10;
-        ctx.lineCap = 'round';
-        ctx.strokeStyle = `${x}`;
-        ctx.lineTo(e.clientX, e.clientY)
+        ctx.moveTo(prevX, prevY);
+        ctx.lineTo(currX, currY);
+        ctx.strokeStyle = x;
+        ctx.lineWidth = y;
         ctx.stroke();
-        ctx.beginPath();
-        ctx.moveTo(e.clientX, e.clientY) 
-        }, 1);
-
+        ctx.closePath();
     }
-
-    //EVENT  LISTENERS
-    canvas.addEventListener('mousedown', startPosition);
-    canvas.addEventListener('mouseup', finishedPosition)
-    canvas.addEventListener('mousemove', draw)
-
-})
+    
+    function erase() {
+        var m = confirm("Want to clear");
+        if (m) {
+            ctx.clearRect(0, 0, w, h);
+            document.getElementById("canvasimg").style.display = "none";
+        }
+    }
+    
+    function save() {
+        document.getElementById("canvasimg").style.border = "2px solid";
+        var dataURL = canvas.toDataURL();
+        document.getElementById("canvasimg").src = dataURL;
+        document.getElementById("canvasimg").style.display = "inline";
+    }
+    
+    function findxy(res, e) {
+        if (res == 'down') {
+            prevX = currX;
+            prevY = currY;
+            currX = e.clientX - canvas.offsetLeft;
+            currY = e.clientY - canvas.offsetTop;
+    
+            flag = true;
+            dot_flag = true;
+            if (dot_flag) {
+                ctx.beginPath();
+                ctx.fillStyle = x;
+                ctx.fillRect(currX, currY, 2, 2);
+                ctx.closePath();
+                dot_flag = false;
+            }
+        }
+        if (res == 'up' || res == "out") {
+            flag = false;
+        }
+        if (res == 'move') {
+            if (flag) {
+                prevX = currX;
+                prevY = currY;
+                currX = e.clientX - canvas.offsetLeft;
+                currY = e.clientY - canvas.offsetTop;
+                draw();
+            }
+        }
+    }
